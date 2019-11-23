8d274435cb712d8b4d7d612dd6c539eb8657e1ac
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-23 11:02:18 -0600

    Se instalaron las dependencias para montar el API en un servidor local. Se removió del esquema el método 'saludo'. Se agregó el comando 'dev' a las dependencias en el archivo package.json. Se terminó la clase: 'Sirviendo el API en la web'.

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+const app = express()
+const port = process.env.port || 3000
 
 //Definir esquema
 const schema = buildSchema(`type Query{
     hello: String
-    saludo: String
 }`)
 
 //Configurar resolvers
 const resolvers = {
     hello: () => {
         return 'Hola mundo'
-    },
-    saludo: () => {
-        return 'Saludo'
     }
 }
 
-// Ejectuar query
-graphql(schema, '{saludo}', resolvers).then((data) => {
-    console.log(data);
+// // Ejectuar query en termial
+// graphql(schema, '{saludo,hello}', resolvers).then((data) => {
+//     console.log(data);
+
+// })
+
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}/api`);
 
 })
-
