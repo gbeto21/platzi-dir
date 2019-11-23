30ab932856cff60d3b3809ca0ef98593aaa37394
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-23 10:22:28 -0600

    Se inicializa el repositorio. Se terminó la clase: 'Schema y types'.

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const { graphql, buildSchema } = require('graphql')
+
+const schema = buildSchema(`type Query{
+    hello: String
+}`)
+
+graphql(schema, '{hello}').then((data) => {
+    console.log(data);
+
+})
+
