21fe253d2a83d79f145b407945ee333229b42b03
Alberto González Balderas <albertoglezb@hotmail.com>
2019-11-23 16:42:59 -0600

    Se crearon los archivos de: 'resolvers.js' y 'schema.graphql'. Se terminó la clase: 'Custom Types'.

--- index.js
+++ index.js
@@ -1,36 +1,29 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers')
+
 const app = express()
 const port = process.env.port || 3000
 
-//Definir esquema
-const schema = buildSchema(`type Query{
-    hello: String
-}`)
-
-//Configurar resolvers
-const resolvers = {
-    hello: () => {
-        return 'Hola mundo'
-    }
-}
-
-// // Ejectuar query en termial
-// graphql(schema, '{saludo,hello}', resolvers).then((data) => {
-//     console.log(data);
-
-// })
+// definiendo el esquema
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
 }))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/api`);
-
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
